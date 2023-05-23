46db9ed0602e0f7928255009c0cd031f9ba1682b
Rex1o <70145948+Rex1o@users.noreply.github.com>
2023-05-23 01:48:24 -0400

    proc presque fini

--- sqlInstance/scripts/proc.sql
+++ sqlInstance/scripts/proc.sql
@@ -1,33 +1,42 @@
-CREATE FUNCTION
+CREATE function
     TABLEAU(
     _debut TIMESTAMP,
      _fin TIMESTAMP,
      _categorie INT)
-    RETURNS TABLE(
-        numero_local INT,
-        pav_id CHAR(3),
-        res_id INT
-    )
+    RETURNS TABLE
+        (
+            _temps timestamp,
+            _numero_local int,
+            _pav_id char(3),
+            _commentaire varchar
+        )
 LANGUAGE plpgsql
 AS $$
+DECLARE
+    _interval INTERVAL;
+    _rowAmount INT;
 BEGIN
-    RAISE NOTICE 'Value: %', ;
-    hello := div(_fin - _debut)
---     SELECT *
---     FROM Reservation as res
---     INNER JOIN local AS loc
---     ON res.numero_local = loc.numero_local
---     AND res.pav_id = loc.pav_id
---     WHERE loc.cat_id = _categorie
---     AND (
---         (res.debut >= _debut AND res.debut <= _fin)
---     OR
---         (res.fin >= _debut AND res.fin <= _fin));
-
-
-
+    _interval := _fin - _debut;
+    _rowAmount :=  (SELECT TRUNC(EXTRACT(EPOCH FROM _interval::INTERVAL)/60) / 15);
+RETURN QUERY
+    SELECT dates.number, loc.numero_local, loc.pav_id,
+           (
+           SELECT res.commentaire
+           FROM reservation as res
+              WHERE dates.number >= res.debut AND dates.number <= res.fin
+               ) commentaire
+    FROM local AS loc
+        CROSS JOIN (
+    SELECT * from generate_series(
+            _debut,_fin,
+            INTERVAL '15 minutes'
+        ) AS number) AS dates
+    WHERE loc.cat_id = _categorie;
 END$$;
 
-SELECT make_timestamp(1997,6,4,14,55,0);
+SELECT *
+FROM local
+         Cross join (SELECT generate_series(0, 6)) as locals
+WHERE local.cat_id = 6;
 
-SELECT * FROM TABLEAU(make_timestamp(1997,6,4,14,55,0),make_timestamp(1997,6,4,18,55,0),3);
+SELECT * FROM TABLEAU(make_timestamp(1997,6,4,14,55,0),make_timestamp(1997,6,4,18,55,0),6);
