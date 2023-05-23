aeb252301782410677c24eede317fd8fcf3378e5
Rex1o <70145948+Rex1o@users.noreply.github.com>
2023-05-22 23:37:16 -0400

    too cool to be true

--- sqlInstance/scripts/proc.sql
+++ sqlInstance/scripts/proc.sql
@@ -1,1 +1,33 @@
-CREATE PROCEDURE TABLEAU
+CREATE FUNCTION
+    TABLEAU(
+    _debut TIMESTAMP,
+     _fin TIMESTAMP,
+     _categorie INT)
+    RETURNS TABLE(
+        numero_local INT,
+        pav_id CHAR(3),
+        res_id INT
+    )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RAISE NOTICE 'Value: %', ;
+    hello := div(_fin - _debut)
+--     SELECT *
+--     FROM Reservation as res
+--     INNER JOIN local AS loc
+--     ON res.numero_local = loc.numero_local
+--     AND res.pav_id = loc.pav_id
+--     WHERE loc.cat_id = _categorie
+--     AND (
+--         (res.debut >= _debut AND res.debut <= _fin)
+--     OR
+--         (res.fin >= _debut AND res.fin <= _fin));
+
+
+
+END$$;
+
+SELECT make_timestamp(1997,6,4,14,55,0);
+
+SELECT * FROM TABLEAU(make_timestamp(1997,6,4,14,55,0),make_timestamp(1997,6,4,18,55,0),3);
